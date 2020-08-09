a6dffa32b641dc6cf73bcd45ae884030212c17e0
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-08-09 10:46:19 +0300

    hw-4/Test::by_category_title added

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,8 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :users_tests, dependent: :destroy
   has_many :users, through: :users_tests
+
+  def self.by_category_title(category_title)
+    joins(:category).where(categories: {title: category_title}).order(id: :desc)
+  end
 end
