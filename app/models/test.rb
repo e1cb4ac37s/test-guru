ef6e07c8a5b66ac9286eb26002955969b648b792
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-08-11 08:25:10 +0300

    hw-5/User can be "author" of many "created_tests", Test can have "author"

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, foreign_key: 'author_id', class_name: 'User'
   has_many :questions, dependent: :destroy
   has_many :users_tests, dependent: :destroy
   has_many :users, through: :users_tests
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_160255) do
+ActiveRecord::Schema.define(version: 2020_08_11_044813) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2020_08_08_160255) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id", null: false
+    t.integer "author_id", null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -64,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_160255) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
   add_foreign_key "users_tests", "tests"
   add_foreign_key "users_tests", "users"
 end
