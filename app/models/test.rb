c17a223e57a91a6a06fe604afcff35ab7e3ebe1a
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-08-12 08:09:21 +0300

    hw-6/test scopes by difficulty added: easy/normal/hard

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,10 @@ class Test < ApplicationRecord
   has_many :users_tests, dependent: :destroy
   has_many :users, through: :users_tests
 
+  scope :easy, -> { where(level: 0..1) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
   def self.by_category_title(category_title)
     joins(:category).where(categories: {title: category_title}).order(id: :desc).pluck(:title)
   end
