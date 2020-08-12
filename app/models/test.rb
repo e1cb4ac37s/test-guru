19ca6b5d07c6816ed5540204dd9693eb23e4484f
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-08-12 08:21:35 +0300

    hw-6/Test.by_category_title is now scope method

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,8 +8,5 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :normal, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-
-  def self.by_category_title(category_title)
-    joins(:category).where(categories: {title: category_title}).order(id: :desc).pluck(:title)
-  end
+  scope :by_category_title, -> (category_title) { joins(:category).where(categories: {title: category_title}).order(id: :desc).pluck(:title) }
 end
