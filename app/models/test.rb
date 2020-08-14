9f56ef71476162ceb45f7f78495c86665e637e3a
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-08-14 06:21:47 +0300

    hw-6/by_category_title scope fixed (should return Collection proxy)

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,8 +8,12 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :normal, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-  scope :by_category_title, -> (category_title) { joins(:category).where(categories: {title: category_title}).order(id: :desc).pluck(:title) }
+  scope :by_category_title, -> (category_title) { joins(:category).where(categories: {title: category_title}).order(id: :desc) }
 
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { greater_than_or_equal_to: 0 }
+
+  def self.titles_by_category(category_title)
+    by_category_title(category_title).pluck(:title)
+  end
 end
