df8299003203a2a5987b42b44b89234aac2a46ca
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-08-19 21:05:06 +0300

    hw-7/fixes after review

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,37 +1,33 @@
 class QuestionsController < ApplicationController
-  before_action :test, except: %i[index, create]
+  before_action :test, only: %i[index create new]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_not_found
 
   def index
-    @questions = test.questions
+    @questions = @test.questions
   end
 
   def show
-    @question = test.questions.find(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def new; end
 
   def create
-    new_question = test.questions.create(body: params[:body])
-    answers = params[:answers].map do |answer|
-      correct = answer[:correct] = 'on' ? true : false
-      Answer.create(
-        body: answer[:body],
-        correct: correct,
-        question: new_question
-      ) if answer[:body]
-    end
+    @test.questions.create(question_params)
     redirect_to action: 'index'
   end
 
   def destroy
-    test.questions.find(params[:id]).destroy
-    redirect_to test_questions_path
+    question = Question.find(params[:id]).destroy
+    redirect_to test_questions_path(question.test)
   end
 
   private
+
+  def question_params
+    params.permit(:body)
+  end
 
   def test
     @test = Test.find(params[:test_id])
