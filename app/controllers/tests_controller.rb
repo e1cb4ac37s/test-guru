8cdbabbcaad36a499e5eceb529bb15374ddcf1ad
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-08-30 11:52:09 +0300

    hw-9/ completed crud for tests (although creation won't work, no author id)

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,10 +1,49 @@
 class TestsController < ApplicationController
+  before_action :set_test, only: %i[show edit]
+
   def index
     @tests = Test.all.preload(:questions)
   end
 
   def show
-    @test = Test.find(params[:id])
     @questions = @test.questions
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    test = Test.new(test_params)
+    if test.save
+      redirect_to test
+    else
+      #
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      #
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def set_test
+    @test = Test.find(params[:id])
   end
 end
