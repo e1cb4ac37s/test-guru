ae9e586a656669b1b13fb0229115274d3e040e3e
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-02 07:58:47 +0300

    hw-9/ actual homework changes

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,9 +1,12 @@
 class TestPassage < ApplicationRecord
+  SUCCESS_BOUNDARY = 85
+
   belongs_to :test
   belongs_to :user
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
 
   def completed?
     current_question.nil?
@@ -11,8 +14,19 @@ class TestPassage < ApplicationRecord
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-    self.current_question = next_question
     save!
+  end
+
+  def progress
+    (correct_questions.to_f / test.questions.count * 100).round(0)
+  end
+
+  def passed?
+    progress >= SUCCESS_BOUNDARY
+  end
+
+  def current_question_number
+    test.questions.index(current_question) + 1
   end
 
   private
@@ -21,17 +35,15 @@ class TestPassage < ApplicationRecord
     self.current_question = test.questions.first if test.present?
   end
 
+  def before_validation_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-    correct_answers_count == correct_answers.where(id: answer_ids).count &&
-      correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct
-  end
-
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
