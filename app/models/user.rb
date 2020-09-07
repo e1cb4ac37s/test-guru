a2f249e9317ffe5ca6c9d729f2f8a605eda02dcc
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-07 07:58:40 +0300

    hw-10/ validate email on correctness & uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
+require_relative './validators/email_validator'
+
 class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :created_tests, foreign_key: 'author_id', class_name: 'Test'
+
+  validates :email, presence: true, uniqueness: true, email: true
 
   has_secure_password
 
