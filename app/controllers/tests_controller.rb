9ceb30990f0b0c28cf55057df158102595b5064c
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-10 08:41:01 +0300

    admin-resources-lesson

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,41 +1,8 @@
 class TestsController < ApplicationController
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_test, only: :start
 
   def index
     @tests = Test.all.preload(:questions)
-  end
-
-  def show
-    @questions = @test.questions
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = Test.new(test_params)
-    @test.author = current_user
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -44,10 +11,6 @@ class TestsController < ApplicationController
   end
 
   private
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id)
-  end
 
   def set_test
     @test = Test.find(params[:id])
