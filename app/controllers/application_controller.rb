47ee7b568fa78569ad9827b7bf82d48586744abb
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-11 07:51:12 +0300

    hw-11/ redirects after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    current_user.is_a?(Admin) ? admin_tests_path : (stored_location_for(resource) || root_path)
+  end
 end
