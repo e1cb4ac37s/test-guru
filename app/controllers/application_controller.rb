1d786dd6198cc9f6471e3cac5d94e1028e64d183
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-11 08:18:02 +0300

    hw-11/ added first/last_name to signup form, permitted thy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :permit_params, if: :devise_controller?
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
     current_user.is_a?(Admin) ? admin_tests_path : (stored_location_for(resource) || root_path)
+  end
+
+  def permit_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
 end
