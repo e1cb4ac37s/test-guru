56a15f98973cf75f7fe1296449e9e1ca367f6fdc
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-11 08:34:27 +0300

    hw-11/ nav shows users first/last_name if ones exist

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
     repo = options[:repo] ? "/#{options[:repo]}" : ''
     link_to body, "https://github.com/#{author}#{repo}", options.except(:repo)
   end
+
+  def appeal(current_user)
+    f, l, e = current_user.first_name, current_user.last_name, current_user.email
+    f && l ? "#{f} #{l}" : e
+  end
 end
