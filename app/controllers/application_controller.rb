3b9cbaa47f3a5f13c56d3ae29d7ebecd7cf65bea
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-14 07:43:03 +0300

    hw-12/ i18n lesson code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :permit_params, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :set_locale
 
   def after_sign_in_path_for(resource)
     current_user.admin? ? admin_tests_path : (stored_location_for(resource) || root_path)
   end
 
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  private
+
   def permit_params
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
