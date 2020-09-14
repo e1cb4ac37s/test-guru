7d1b4c2b9d8bde466273e484e1bb7fb2b810dbfd
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-14 22:30:38 +0300

    hw-12/ whole lot of i18n

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,6 @@
 module QuestionsHelper
   def question_header(test, question)
-    "#{question.persisted? ? 'Edit' : 'Create new'} \"#{test.title}\" question"
+    t('.header', test_title: test.title)
+    # "#{question.persisted? ? 'Edit' : 'Create new'} \"#{test.title}\" question"
   end
 end
