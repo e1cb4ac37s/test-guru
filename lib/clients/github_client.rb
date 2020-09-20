a10478673812043abea09fee2e1abb39b1e5bf37
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-20 09:00:46 +0300

    hw-14/ moved api key to encrypted file

--- lib/clients/github_client.rb
+++ lib/clients/github_client.rb
@@ -1,6 +1,5 @@
 class GithubClient
   ROOT_ENDPOINT = 'https://api.github.com'
-  ACCESS_TOKEN = '12fba180662d98cd673a7073558eaa7250006224'
 
   def initialize
     @http_client = setup_http_client
@@ -8,7 +7,7 @@ class GithubClient
 
   def create_gist(params)
     @http_client.post('gists') do |request|
-      request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
+      request.headers['Authorization'] = "token #{Rails.application.credentials.github_api_key}"
       request.headers['Content-Type'] = 'application/json'
       request.body = params.to_json
     end
