6a8fdd008b09b63e60e4548105b1d03c0d0cb153
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-09-30 08:03:22 +0300

    hw-17/ --

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test_guru_production"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'mighty-sierra-64935.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     domain: 'mighty-sierra-64935.herokuapp.com',
